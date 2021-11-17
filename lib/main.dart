cc69c0bf2141e0c74fa7b86f9b36623bff3db133
Brunohenriquee <henriquebrunoop@gmail.com>
2021-11-17 20:03:43 -0300

    Atualização

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:calculadora/homeapp.dart';
+import 'package:flutter/material.dart';
+void  main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  HomeApp(),
+    )
+  );
+}
